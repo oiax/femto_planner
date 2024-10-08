6d7cc6530da78a4c94790d3563529e532d36772f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-10-08 20:41:50 +0900

    Seeding script for PlanItem

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,7 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     FemtoPlanner.Repo.insert!(%FemtoPlanner.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias FemtoPlanner.Repo
+alias FemtoPlanner.Schedule.PlanItem
+
+Repo.insert!(%PlanItem{
+  name: "読書",
+  description: "『走れメロス』を読む"
+})
