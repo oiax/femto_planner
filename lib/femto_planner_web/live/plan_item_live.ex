1308f941fde6a7b576ccb99ec188b50a59659d25
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-10-15 09:25:46 +0900

    Format date and time

--- lib/femto_planner_web/live/plan_item_live.ex
+++ lib/femto_planner_web/live/plan_item_live.ex
@@ -1,5 +1,6 @@
 defmodule FemtoPlannerWeb.PlanItemLive do
   use FemtoPlannerWeb, :live_view
+  import Calendar, only: [strftime: 2]
   alias FemtoPlanner.Schedule
 
   embed_templates "plan_item_live/*"
@@ -25,14 +26,36 @@ defmodule FemtoPlannerWeb.PlanItemLive do
     """
   end
 
-  defp duration(%{item: item}) do
-    assigns = %{
-      starts_at: item.starts_at,
-      ends_at: item.ends_at
-    }
-
+  defp duration(assigns) do
     ~H"""
-      <%= @starts_at %> 〜 <%= @ends_at %>
+      <%= format_starts_at(@item) %>
+      〜
+      <%= format_ends_at(@item) %>
     """
+  end
+
+  @time_zone Application.compile_env(:femto_planner, :default_time_zone)
+
+  defp format_starts_at(item) do
+    if item.starts_at.year == DateTime.now!(@time_zone).year do
+      strftime(item.starts_at, "%-m月%-d日 %H:%M")
+    else
+      strftime(item.starts_at, "%Y年%-m月%-d日 %H:%M")
+    end
+  end
+
+  defp format_ends_at(item) do
+    import DateTime, only: [to_date: 1]
+
+    cond do
+      to_date(item.starts_at) == to_date(item.ends_at) ->
+        strftime(item.ends_at, "%H:%M")
+
+      item.starts_at.year == item.ends_at.year ->
+        strftime(item.ends_at, "%-m月%-d日 %H:%M")
+
+      true ->
+        strftime(item.ends_at, "%Y年%-m月%-d日 %H:%M")
+    end
   end
 end
