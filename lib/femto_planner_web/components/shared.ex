a2ba470f2c680a5cfe20feef0ec7ff1193d122e1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-11-05 11:25:49 +0900

    Refactor new.html.heex using Shared.text_input/1

--- lib/femto_planner_web/components/shared.ex
+++ lib/femto_planner_web/components/shared.ex
@@ -8,4 +8,22 @@ defmodule FemtoPlannerWeb.Shared do
     <span class="material-symbols-outlined"><%= @name %></span>
     """
   end
+
+  attr :field, Phoenix.HTML.FormField, required: true
+  attr :label, :string, required: true
+
+  def text_input(assigns) do
+    ~H"""
+    <div class="form-control p-4">
+      <label for={@field.id} class="font-bold"><%= @label %></label>
+      <input
+        type="text"
+        id={@field.id}
+        name={@field.name}
+        value={@field.value}
+        class="input input-bordered border-gray-500"
+      />
+    </div>
+    """
+  end
 end
