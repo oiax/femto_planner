8448cd159c9088af5d360d0c2833b05f2e7bc35f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-11-29 22:35:17 +0900

    3-8: Shared.date_and_time_input/1

--- lib/femto_planner_web/components/shared.ex
+++ lib/femto_planner_web/components/shared.ex
@@ -57,4 +57,46 @@ defmodule FemtoPlannerWeb.Shared do
     </div>
     """
   end
+
+  attr :date_field, Phoenix.HTML.FormField, required: true
+  attr :hour_field, Phoenix.HTML.FormField, required: true
+  attr :minute_field, Phoenix.HTML.FormField, required: true
+  attr :label, :string, required: true
+
+  def date_and_time_input(assigns) do
+    ~H"""
+    <div class="form-control p-4">
+      <div>
+        <label for={@date_field.id} class="font-bold"><%= @label %></label>
+      </div>
+      <div>
+        <input
+          type="date"
+          id={@date_field.id}
+          name={@date_field.name}
+          value={@date_field.value}
+          class="input input-bordered border-gray-500"
+        />
+        <select
+          name={@hour_field.name}
+          class="select select-bordered border-gray-500 ml-1"
+        >
+          <%= Phoenix.HTML.Form.options_for_select(
+            0..23,
+            @hour_field.value
+          ) %>
+        </select>
+        <select
+          name={@minute_field.name}
+          class="select select-bordered border-gray-500 ml-1"
+        >
+          <%= Phoenix.HTML.Form.options_for_select(
+            0..55//5,
+            @minute_field.value
+          ) %>
+        </select>
+      </div>
+    </div>
+    """
+  end
 end
