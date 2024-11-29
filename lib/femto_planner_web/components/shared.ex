9de5fba247e791661ac99a14484de34640e73210
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-11-29 16:00:42 +0900

    3-4: Shared.textarea/1

--- lib/femto_planner_web/components/shared.ex
+++ lib/femto_planner_web/components/shared.ex
@@ -34,4 +34,27 @@ defmodule FemtoPlannerWeb.Shared do
     </div>
     """
   end
+
+  attr :field, Phoenix.HTML.FormField, required: true
+  attr :label, :string, required: true
+  attr :optional, :boolean
+
+  def textarea(assigns) do
+    assigns = assign_new(assigns, :optional, fn -> false end)
+
+    ~H"""
+    <div class="form-control p-4">
+      <div>
+        <label for={@field.id} class="font-bold"><%= @label %></label>
+        <%= if @optional do %>
+          <small>(Optional)</small>
+        <% end %>
+      </div>
+      <textarea
+        id={@field.id}
+        class="textarea textarea-bordered border-gray-500"
+      ><%= @field.value %></textarea>
+    </div>
+    """
+  end
 end
