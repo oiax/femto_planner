d82ede0b39ea1b18eb69abf272a23382a5060e56
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-11-29 15:51:23 +0900

    3-4: Shared.text_input

--- lib/femto_planner_web/components/shared.ex
+++ lib/femto_planner_web/components/shared.ex
@@ -8,4 +8,30 @@ defmodule FemtoPlannerWeb.Shared do
     <span class="material-symbols-outlined"><%= @name %></span>
     """
   end
+
+  attr :field, Phoenix.HTML.FormField, required: true
+  attr :label, :string, required: true
+  attr :optional, :boolean
+
+  def text_input(assigns) do
+    assigns = assign_new(assigns, :optional, fn -> false end)
+
+    ~H"""
+    <div class="form-control p-4">
+      <div>
+        <label for={@field.id} class="font-bold"><%= @label %></label>
+        <%= if @optional do %>
+          <small>(Optional)</small>
+        <% end %>
+      </div>
+      <input
+        type="text"
+        id={@field.id}
+        name={@field.name}
+        value={@field.value}
+        class="input input-bordered border-gray-500"
+      />
+    </div>
+    """
+  end
 end
